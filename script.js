f8cd8c2199a56571112e51349f523e5a45a10c8f
itCanelita <abelastacio86@gmail.com>
2024-02-26 23:57:35 -0400

    Waniel te voy a da

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 
 const btnIniciar = document.querySelector("#btn-iniciar")
 const icono = document.querySelector("#icono")
+const login = document.querySelector("#login")
+const alert = document.querySelector("#alert")
 
 btnIniciar.addEventListener('click', ()=> {
+    icono.classList.remove("vueltica")
+    login.classList.remove("error")
+    alert.classList.remove("alertA")
+    void login.offsetWidth;
     icono.classList.add("vueltica")
-
-    setTimeout(function(){
-        icono.classList.remove("vueltica")
-    }, 1600);
+    login.classList.add("error")
+    alert.classList.add("alertA")
 })
