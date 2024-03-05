8daabe9b67db62b56384520af19c71aedf2acb3a
itCanelita <abelastacio86@gmail.com>
2024-03-05 17:25:52 -0400

    Ajuste de animacion

--- script.js
+++ script.js
@@ -5,11 +5,9 @@ const login = document.querySelector("#login")
 const alert = document.querySelector("#alert")
 
 btnIniciar.addEventListener('click', ()=> {
-    icono.classList.remove("vueltica")
     login.classList.remove("error")
     alert.classList.remove("alertA")
     void login.offsetWidth;
-    icono.classList.add("vueltica")
     login.classList.add("error")
     alert.classList.add("alertA")
 })
